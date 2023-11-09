05431138e0abe2bc1f52c543334b87d90eb6dda4
elisacris98 <cristina.elisa98@gmail.com>
2023-11-09 14:38:43 -0500

    worked on it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const digimon_url = 'https://digimon-api.vercel.app/api/digimon';
+
+
+const DigimonModal = () => {
+  const [digimons, setDigimons] = useState([]);
+  const [digi, setDigi] = useState(null);
+
+  const fetchDigimon = async () => {
+    const response = await fetch(digimon_url);
+    const responseJson = await response.json();
+    setDigimons(responseJson);
+  };
+
+  const displayDigimon = () => {
+    return (
+      <div className="digimons">
+        {digimons.map((digimon) => (
+          <div className="digimon" key={digimon.name}>
+            <img name={digimon.name} src={digimon.img} />
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  const handleClick = async (event) => {
+    const digiUrl = `${digimon_url}/name/${event.target.name}`;
+    const response = await fetch(digiUrl);
+    const responseJson = await response.json();
+    setDigi(responseJson[0]);
+  };
+
+  const openDigimon = async () => {
+    const modal = document.querySelector(".modal");
+    modal.style.display = "block";
+  };
+
+  const closeModal = async () => {
+    const modal = document.querySelector(".modal");
+    modal.style.display = "none";
+  };
+
+  useEffect(() => {
+    fetchDigimon();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {displayDigimon()}
+      <div className="modal">
+        <div className="modal-content">
+          <h3>{digi?.name}</h3>
+          <img src={digi?.img} alt={digi?.name} />
+          <p>Level: {digi?.level}</p>
+        </div>
+        <button className="close-button" onClick={closeModal}>
+          Close
+        </button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default DigimonModal;
